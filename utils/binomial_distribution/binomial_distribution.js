8073a279455ce2efa6ab06cec1312bcf3f53ce4c
king_lel_HD <fischernetzselentjunior@gmail.com>
2025-05-12 00:13:02 +0200

    larger visuals

--- utils/binomial_distribution/binomial_distribution.js
+++ utils/binomial_distribution/binomial_distribution.js
@@ -2,9 +2,12 @@ const canvas = document.getElementById('histogram')
 const ctx = canvas.getContext('2d')
 let width = document.getElementById('input-form').clientWidth
 canvas.width = width - 24
+canvas.style.width = width + 'px'
 window.addEventListener('resize', () => {
     let width = document.getElementById('input-form').clientWidth
     canvas.width = width - 24
+    canvas.style.width = width + 'px'
+    calc()
 })
 
 function fact(n) {
@@ -28,22 +31,32 @@ function calc() {
     const kmax = document.getElementById('k-max').value
     const barWidth = canvas.width / (n + 1)
     let ins = 0
+    let P_array = []
+    let max = 0
     for(let i = 0; i < n + 1; i++){
         let P = PX(n, p, i)
+        if (i >= kmin && i <= kmax) ins += P
+        P_array.push(P)
+        if (P * canvas.height > max) max = P * canvas.height
+    }
+    const scale = canvas.height / max * 0.8
+    for(let i = 0; i < P_array.length; i++){
+        let P = P_array[i]
         if (i >= kmin && i <= kmax) {
-            ins += P
+            ctx.fillStyle = 'rgba(123, 104, 238, 0.25)'
+            ctx.fillRect(i*barWidth, 0, barWidth, canvas.height)
             ctx.fillStyle = 'rgb(123, 104, 238)'
         } else {
             ctx.fillStyle = 'gray'
         }
-        let height = canvas.height * P
+        let height = canvas.height * P * scale
         ctx.fillRect(i*barWidth, canvas.height - height, barWidth, height)
         ctx.fillStyle = 'black'
         ctx.fillRect(i*barWidth, 0, 1, canvas.height)
         ctx.fillRect(i*barWidth, 0, 1, canvas.height)
     }
     document.getElementById('ans').style.display = 'block'
-    document.getElementById('ans').innerText = 'P(X) = ' + Math.floor(ins * 100000) / 100000
+    document.getElementById('ans').innerText = `P(${kmin == kmax ? "X = " + kmin : kmin + " ≤ X ≤ " + kmax}) = ` + Math.floor(ins * 100000) / 100000
 }
 
 calc()
